446d16cac230957f266fa6c2baa83bed176bdde1
mjuniper <mjuniper@gmail.com>
2014-03-12 15:51:58 -0700

    Updates

---
+++ scripts/bb7a86fa.main.js
@@ -0,0 +1,1 @@
+window.nickDemo={Models:{},Collections:{},Views:{},Routers:{},init:function(){"use strict";console.log("Hello from Backbone!")}},$(document).ready(function(){"use strict";nickDemo.init()}),this.JST=this.JST||{};
